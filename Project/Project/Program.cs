68285df246a6160f5c6f083c90ba18070bf14d9a
Yordanos Dereje <yordadereje@gmail.com>
2022-06-25 18:19:54 +0300

    Corrections

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -3,11 +3,12 @@
  * It has 5 operations (+, -, *, /, %)
  */
 double val1, val2, val3;
-String v1, v2, v3;
-int op;
-char ans;
+String v1, v2;
+char op, ans;
+bool isThere;
 do
 {
+    Console.Clear();
     do
     {
         Console.WriteLine("Enter the first value");
@@ -16,59 +17,62 @@ do
         v2 = Console.ReadLine();
         if (Double.TryParse(v1, out val1) == false || Double.TryParse(v2, out val2) == false)
         {
-            Console.WriteLine("Please be sure to only enter numbericals");
+            Console.WriteLine("Please be sure to only enter numericals");
         }
     }while(Double.TryParse(v1, out val1) == false || Double.TryParse(v2, out val2) == false);
 
     val1 = double.Parse(v1);
     val2 = double.Parse(v2);
 
-    Console.WriteLine("Please choose an operation of choice: ");
-    Console.WriteLine("1. Add");
-    Console.WriteLine("2. Subtract");
-    Console.WriteLine("3. Multiply");
-    Console.WriteLine("4. Divide");
-    Console.WriteLine("5. Modulo");
-    op = int.Parse(Console.ReadLine());
-    if (op == 1)
+    do
     {
-        val3 = val1 + val2;
-        v3 = val3.ToString();
-        Console.WriteLine(v1 + " + " + v2 + " = " + v3);
-    }
-    else if (op == 2)
-    {
-        val3 = val1 - val2;
-        v3 = val3.ToString();
-        Console.WriteLine(v1 + " - " + v2 + " = " + v3);
-    }
-    else if (op == 3)
-    {
-        val3 = val1 * val2;
-        v3 = val3.ToString();
-        Console.WriteLine(v1 + " * " + v2 + " = " + v3);
-    }
-    else if (op == 4)
-    {
-        do
+        isThere = true;
+        Console.WriteLine("Please choose an operation of choice: ");
+        Console.WriteLine("+  -   *  /  %");
+        op = char.Parse(Console.ReadLine());
+        if (op == '+')
         {
-            if (val2 == 0)
+            val3 = val1 + val2;
+            Console.WriteLine(val3.ToString());
+        }
+        else if (op == '-')
+        {
+            val3 = val1 - val2;
+            Console.WriteLine(val3.ToString());
+        }
+        else if (op == '*')
+        {
+            val3 = val1 * val2;
+            Console.WriteLine(val3.ToString());
+        }
+        else if (op == '/')
+        {
+            do
             {
-                Console.WriteLine("Please enter a number other than 0 for the second value");
-                v2 = Console.ReadLine();
-                val2 = double.Parse(v2);
-            }
-        } while (val2 == 0);
-        val3 = val1 / val2;
-        Console.WriteLine(v1 + " * " + v2 + " = " + val3);
-    }
-    else if (op == 5)
-    {
-        val3 = val1 % val2;
-        v3 = val3.ToString();
-        Console.WriteLine(v1 + " % " + v2 + " = " + v3);
-    }
-    Console.WriteLine("Do you want to go again?(Y/y for yes, any other character for no");
+                if (val2 == 0)
+                {
+                    Console.WriteLine("Please enter a number (other than 0) for the second value");
+                    v2 = Console.ReadLine();
+                    
+                }
+            } while (v2.Equals("0") || Double.TryParse(v2, out val2) == false);
+
+            val2 = double.Parse(v2);
+            val3 = val1 / val2;
+            Console.WriteLine(val3.ToString());
+        }
+        else if (op == '%')
+        {
+            val3 = val1 % val2;
+            Console.WriteLine(val3.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+            isThere = false;
+        }
+    } while (!isThere);
+    Console.WriteLine("Go again?(Y/y for yes)");
     ans = char.Parse(Console.ReadLine());
 } while (ans == 'Y' || ans == 'y');
 
